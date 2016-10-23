f21a99f51f87ed2cb4c3b5f913525b8be4f266a3
Giorgi <sheklashviligiorgi@gmail.com>
2016-10-23 22:13:51 +0400

    Details & top rated movies

--- app/src/main/java/ge/gita/restexample/ui/MainActivity.java
+++ app/src/main/java/ge/gita/restexample/ui/MainActivity.java
@@ -1,8 +1,11 @@
 package ge.gita.restexample.ui;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -12,11 +15,9 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
-import java.util.List;
 
 import ge.gita.restexample.dataprovider.Constants;
 import ge.gita.restexample.R;
-import ge.gita.restexample.dataprovider.model.Movie;
 import ge.gita.restexample.dataprovider.model.MovieResponse;
 import ge.gita.restexample.retrofit.ApiClient;
 import ge.gita.restexample.retrofit.ApiInterface;
@@ -25,72 +26,77 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ItemClickListener {
 
     //https://developers.themoviedb.org/3/getting-started
     //https://square.github.io/retrofit/
     //https://futurestud.io/tutorials/retrofit-getting-started-and-android-client
 
-    private TextView resultTextView,textView;
+    private TextView textView;
     private EditText searchEditText;
     private ImageView imageView;
-    private ProgressDialog pd;
+    RecyclerView recyclerView;
     int counter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView=(TextView)findViewById(R.id.textview);
-        resultTextView = (TextView) findViewById(R.id.tv_result);
+        textView = (TextView) findViewById(R.id.textview);
         searchEditText = (EditText) findViewById(R.id.et_search);
-        imageView=(ImageView)findViewById(R.id.imageView);
-        counter=0;
+        imageView = (ImageView) findViewById(R.id.imageView);
+        counter = 0;
         textView.setText(String.valueOf(counter));
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     public void onGetDataClick(View view) {
-        int searh = Integer.parseInt(searchEditText.getText().toString().trim());
-        if (TextUtils.isEmpty(searchEditText.getText().toString().trim())) {
+        imageView.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.VISIBLE);
+        String searh = searchEditText.getText().toString().trim();
+        if (TextUtils.isEmpty(searh)) {
             searchEditText.setError("Enter Search Text");
             return;
         }
-
         ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
-        Call<Movie> call=apiService.getSearchedDetails(searh,Constants.API_KEY);
-        call.enqueue(new Callback<Movie>() {
-            @Override
-            public void onResponse(Call<Movie> call, final Response<Movie> response) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        if(response.body()!=null){
-                            Movie movie=response.body();
-                            resultTextView.setText(movie.getOriginalTitle()+"");
-                            Picasso.with(MainActivity.this).load("http://image.tmdb.org/t/p/w500/"+movie.getPosterPath()).into(imageView);
-                        }
-                    }
-                });
-            }
-
-            @Override
-            public void onFailure(Call<Movie> call, Throwable t) {
-                Log.e("GITA-ANDROID", t.getMessage());
-            }
-        });
-    }
-
-    public void down(View view) {
-        ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
-        Call<MovieResponse> call=apiService.getTopMovies(Constants.API_KEY);
+        Call<MovieResponse> call = apiService.getSearchedMovies(Constants.API_KEY, searh);
         call.enqueue(new Callback<MovieResponse>() {
             @Override
             public void onResponse(Call<MovieResponse> call, final Response<MovieResponse> response) {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        if(response.body()!=null){
-                            MovieResponse movieResponse=response.body();
-                            Picasso.with(MainActivity.this).load("http://image.tmdb.org/t/p/w500/"+movieResponse.getMovieList().get(--counter).getPosterPath()).into(imageView);
+                        if (response.body() != null) {
+                            MovieResponse movie = response.body();
+                            recyclerView.setAdapter(new MovieAdapter(movie.getMovieList(), MainActivity.this, MainActivity.this));
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(Call<MovieResponse> call, Throwable t) {
+                Log.e("GITA-ANDROID", t.getMessage());
+            }
+        });
+    }
+
+    public void down(View view) {
+        recyclerView.setVisibility(View.GONE);
+        imageView.setVisibility(View.VISIBLE);
+
+        ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
+        Call<MovieResponse> call = apiService.getTopMovies(Constants.API_KEY);
+        call.enqueue(new Callback<MovieResponse>() {
+            @Override
+            public void onResponse(Call<MovieResponse> call, final Response<MovieResponse> response) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (response.body() != null) {
+                            MovieResponse movieResponse = response.body();
+                            Picasso.with(MainActivity.this).load("http://image.tmdb.org/t/p/w500/" + movieResponse.getMovieList().get(--counter).getPosterPath()).into(imageView);
                             textView.setText(String.valueOf(counter));
 
                         }
@@ -107,18 +113,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void up(View view) {
-
+        recyclerView.setVisibility(View.GONE);
+        imageView.setVisibility(View.VISIBLE);
         ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
-        Call<MovieResponse> call=apiService.getTopMovies(Constants.API_KEY);
+        Call<MovieResponse> call = apiService.getTopMovies(Constants.API_KEY);
         call.enqueue(new Callback<MovieResponse>() {
             @Override
             public void onResponse(Call<MovieResponse> call, final Response<MovieResponse> response) {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        if(response.body()!=null){
-                            MovieResponse movieResponse=response.body();
-                            Picasso.with(MainActivity.this).load("http://image.tmdb.org/t/p/w500/"+movieResponse.getMovieList().get(++counter).getPosterPath()).into(imageView);
+                        if (response.body() != null) {
+                            MovieResponse movieResponse = response.body();
+                            Picasso.with(MainActivity.this).load("http://image.tmdb.org/t/p/w500/" + movieResponse.getMovieList().get(++counter).getPosterPath()).into(imageView);
                             textView.setText(String.valueOf(counter));
                         }
                     }
@@ -130,5 +137,13 @@ public class MainActivity extends AppCompatActivity {
                 Log.e("GITA-ANDROID", t.getMessage());
             }
         });
+    }
+
+
+    @Override
+    public void onItemClicked(int position) {
+        Intent i = new Intent(MainActivity.this, DetailsActivity.class);
+        i.putExtra("id", position);
+        startActivity(i);
     }
 }
